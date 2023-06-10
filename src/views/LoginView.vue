c4ee05253ab36ad4dc78b5475e5709b29bf49472
fvalgreen <valverde130495@gmail.com>
2023-06-09 21:38:03 -0300

    assets(feat): icons added, loginView(feat): basic structure

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,43 @@
 <script lang="ts">
   import {defineComponent} from 'vue';
-  export default defineComponent({})
+  export default defineComponent({
+    data(){
+      return{
+        login: '',
+        senha: '',
+        loading: false,
+        erro: ''
+      }
+    },
+    methods: {
+      efetuarLogin(){
+        if(!this.login && !this.senha){
+          this.erro = 'Favor preencher o formulário';
+          return;
+        }
+        alert('Login efetuado: ' + this.login + " " + this.senha)
+      }
+    }
+  })
 </script>
 <template>
-  <h1>Bem-vindo ao Devagram</h1>
+  <div>
+    <img src="../assets/imagens/logo.svg" alt="Logo Devagram">
+    <form >
+      <p v-if="erro">{{ erro }}</p>
+      <div>
+        <img src="../assets/imagens/envelope.svg" alt="Insira o login" />
+        <input type="email" v-model="login" />
+      </div>
+      <div>
+        <img src="../assets/imagens/chave.svg" alt="Insira a senha" />
+        <input type="password" v-model="senha" />
+      </div>
+      <button @click.enter.prevent="efetuarLogin">Login</button>
+      <div>
+        <p>Não possui uma conta?</p>
+        <a href="#">Faça seu cadastro agora!</a>
+      </div>
+    </form>
+  </div>
 </template>
