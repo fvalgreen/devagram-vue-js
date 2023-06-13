c6e152af9602135635c79b7290bb3bc6b6f83127
fvalgreen <valverde130495@gmail.com>
2023-06-13 15:19:24 -0300

    feat(view): HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import router from '@/router';
+  import { LoginServices } from '@/services/LoginServices';
+  import { defineComponent } from 'vue';
+
+  const loginServices = new LoginServices;
+  export default defineComponent({   
+    setup(){
+      return {
+        loginServices
+      }
+    },
+    methods: {
+      sair(){
+        loginServices.logout();
+        router.push({name: "login"})  
+        
+      }
+    }
+  })
+</script>
+<template>
+  <div>
+    <button @click.prevent="sair">Sair</button>
+  </div>
+</template>
