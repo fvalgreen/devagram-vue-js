a79df5327f7b8963e021f178ff1dbabefab7419b
fvalgreen <valverde130495@gmail.com>
2023-06-13 15:17:43 -0300

    feat(src): route public and private

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import HomeView from '../views/HomeView.vue'
+import { useAccessTokenStore } from '@/stores/accessToken';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {rotaPrivada: true}
+    },    
+    {
+      path: '/',
       name: 'login',
       component: LoginView
-    },
-    
+    }
   ]
-})
+});
+
+router.beforeEach((to, from) => {
+  const store = useAccessTokenStore();
+  if(to.name !== 'login' && to.meta.rotaPrivada && !store.estaAutenticado){
+    return {name: 'login'}
+  }else if(to.name === 'cadastro' && store.estaAutenticado){
+    return {name: 'home'}
+  }
+});
 
 export default router
