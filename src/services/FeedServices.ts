4804b48a414c55b26f31f4f2d2a10b20777d3954
fvalgreen <valverde130495@gmail.com>
2023-06-15 10:28:24 -0300

    feat(services): FeedServices

---
+++ src/services/FeedServices.ts
@@ -0,0 +1,10 @@
+import { HttpApiServices } from "./HttpApiServices";
+
+export class FeedServices extends HttpApiServices{
+  async getFeedPrincipal(){
+    return await this.get('/feed');
+  }
+  async getFeedPorId(id: String){
+    return await this.get('/feed?id=' + id);
+  }
+}
