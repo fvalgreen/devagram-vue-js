3ee086803dd0b0ce94c442ac8e0ee89989f029b7
fvalgreen <valverde130495@gmail.com>
2023-06-15 11:04:42 -0300

    feat(components): Avatar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import avatarImagem from '../assets/imagens/avatar.svg';
+  export default defineComponent({
+    props:{
+      imagem: String
+    },
+    computed:{
+      escolherAvatar(): string{
+        return this.imagem ? this.imagem : avatarImagem
+      }
+    }
+})
+</script>
+<template>
+  <img :src="escolherAvatar" alt="Foto usuário" class="avatar">
+</template>
+<style lang="scss" src="@/assets/styles/footer.scss" />
