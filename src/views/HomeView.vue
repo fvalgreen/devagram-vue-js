509130b03a1866ad927827b2900cf5aab9d389b4
fvalgreen <valverde130495@gmail.com>
2023-06-15 10:28:55 -0300

    feat(components): Postagem and Feed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,32 @@
   import { defineComponent } from 'vue';
   import HeaderVue from '@/components/Header.vue';
   import FooterVue from '@/components/Footer.vue';
+  import {FeedServices} from '@/services/FeedServices';
+  import Feed from '@/components/Feed.vue';
+
+  const feedServices = new FeedServices();
 
   export default defineComponent({   
-    components: { HeaderVue, FooterVue }
+    data(){
+      return{
+        posts: []
+      }
+    },
+    components: { HeaderVue, FooterVue, Feed },
+    async mounted(){
+      try {
+        const resultado = await feedServices.getFeedPrincipal()
+        if(resultado && resultado.data){
+          this.posts = resultado.data
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
   })
 </script>
 <template>
   <HeaderVue />
+  <Feed :posts="posts"/>
   <FooterVue />
 </template>
